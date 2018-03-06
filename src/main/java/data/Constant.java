d30ba4af98e409326aacdd4c325568b5716c934d
Omar Meneses <omeneses@H5613.ad.hugeinc.com>
2018-03-06 09:49:39 -0500

    upd the constant class

--- src/main/java/data/Constant.java
+++ src/main/java/data/Constant.java
@@ -1,8 +1,6 @@
 package data;
 
 public interface Constant {
-    public static final String URL_QA = "https://qa.fourseasons.hugeops.com/content/fshr/pages/en/properties/surfside/";
-    public static final String URL_CAMPAIGNS = "https://qa.fourseasons.hugeops.com/content/fshr/pages/en/campaigns/campaign_feb";
-    public static final String URL_TRAVELOCITY = "https://www.travelocity.com";
+     public static final String URL_TRAVELOCITY = "https://www.travelocity.com";
 
 }
