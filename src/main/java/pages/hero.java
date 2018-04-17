da43ba1c26b3bce95fc0d5a65ed1fdc32a0bdb3d
Omar Meneses <omeneses@hugeinc.com>
2018-04-17 14:58:47 -0500

    class HomePage  renamed and some tests added

--- src/main/java/pages/hero.java
+++ src/main/java/pages/hero.java
@@ -10,9 +10,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.CommonFuntions;
 
-public class HomePageFS {
+public class hero {
     //Methods to interact on all page's elements
-    public GalleryOverlay goGalleryOverlay(WebDriver driver) throws Exception {
+    public GalleryOverlay openGalleryOverlay(WebDriver driver) throws Exception {
         CommonFuntions.clickOnAnyElement(driver,By.xpath("//div[@class='Gallery Hero-gallery']/button[@class='Button--underlined--dark Gallery-button']/span[@class='Button-label']"),Constant.TIME_OUT);
         return new GalleryOverlay();
     }
@@ -21,7 +21,28 @@ public class HomePageFS {
         return CommonFuntions.getTextOfAnyElement(driver,By.xpath("//div[@class='Gallery Hero-gallery']/button[@class='Button--underlined--dark Gallery-button']/span[@class='Button-label']"),Constant.TIME_OUT);
     }
 
-    //Locating all private page's elements
+    public String getHeroEyebrowText(WebDriver driver) throws Exception {
+        return CommonFuntions.getTextOfAnyElement(driver,By.cssSelector("span.Heading-eyebrow"),Constant.TIME_OUT);
+    }
+
+    public String getHeroTitleText(WebDriver driver) throws Exception {
+        return CommonFuntions.getTextOfAnyElement(driver,By.cssSelector("h1.Heading-title"),Constant.TIME_OUT);
+    }
+
+    public String getHeroDescriptionText(WebDriver driver) throws Exception {
+        return CommonFuntions.getTextOfAnyElement(driver,By.cssSelector("span.Heading-subtitle"),Constant.TIME_OUT);
+    }
+
+     //Locating all private page's elements
     @FindBy(xpath = "//div[@class='Gallery Hero-gallery']/button[@class='Button--underlined--dark Gallery-button']/span[@class='Button-label']")
-    private WebElement gallery_link;
+    private WebElement heroGalleryLink;
+
+    @FindBy(css = "span.Heading-eyebrow")
+    private WebElement heroEyebrow;
+
+    @FindBy(css = "h1.Heading-title")
+    private WebElement heroTitle;
+
+    @FindBy(css = "span.Heading-subtitle")
+    private WebElement heroDescription;
 }
