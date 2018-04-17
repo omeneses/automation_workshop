e704572bd0c0334b5a0de12a10969c297b790704
Omar Meneses <omeneses@hugeinc.com>
2018-04-17 15:35:49 -0500

    updated the URL FS

--- src/main/java/data/Constant.java
+++ src/main/java/data/Constant.java
@@ -1,7 +1,7 @@
 package data;
 
 public interface Constant {
-     public static final String URL_FS = "https://wwww.fourseasons.com/content/fshr/pages/en/properties/houston";
+     public static final String URL_FS = "https://www.fourseasons.com/content/fshr/pages/en/properties/houston";
      public static final String GALLERY_LINK_TEXT = "GALLERY";
      public static final String FIRST_IMAGE = "1";
      public static final String SECOND_IMAGE = "2";
